7633b34861a3f8ecba5ddc4b80270c5c8d8cd5f6
Peter Baker <peter@finack.com>
2024-06-30 18:26:29 -0700

    wip

--- app/root.tsx
+++ app/root.tsx
@@ -1,37 +1,40 @@
 import {
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
 } from "@remix-run/react";
 
 import type { LinksFunction } from "@remix-run/node";
 import stylesheet from "~/tailwind.css?url";
 
 export const links: LinksFunction = () => [
-	{ rel: "stylesheet", href: stylesheet },
-	{ rel: "stylesheet", href: "https://rsms.me/inter/inter.css" }, // https://rsms.me/inter/#features
+  { rel: "stylesheet", href: stylesheet },
+  { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" }, // https://rsms.me/inter/#features
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<html className="bg-white lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body className="h-full">
-				{children}
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	);
+  return (
+    <html
+      className="bg-white lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950"
+      lang="en"
+    >
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
-	return <Outlet />;
+  return <Outlet />;
 }
